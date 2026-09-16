2ceeca56d52f5486cd212b229c49dbee5879ee33
JP <dev@jptstar.com>
2026-09-16 23:13:48 +0200

    diagnostic: share Python package version contract

---
+++ tools/tsun_diagnostic_version.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Copyright (C) 2026 Jean-Philippe TESTART (jptstar)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Version contract for the full TSUN Local Diagnostic distribution."""
+
+APP_VERSION = "1.5.21"
